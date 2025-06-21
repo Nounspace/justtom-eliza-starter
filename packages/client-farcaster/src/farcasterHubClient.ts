f8e53804a7851ca6a60c3b29e32e510e303543a9
rferrari <rferrariwd@gmail.com@>
2025-06-21 20:02:35 +0800

    fix: new line at nounspace clanker page to see it embbed it

--- packages/client-farcaster/src/farcasterHubClient.ts
+++ packages/client-farcaster/src/farcasterHubClient.ts
@@ -1318,7 +1318,7 @@ export class FarcasterHubClient {
         const username = deployerInfo.username;
         const bio = deployerInfo.profile.bio.text;
         const nounspacePage = `https://nounspace.com/t/base/${contractAddress}
-        
+
         `;// try new lines after the link to see if neynar embbed it.const thread_hash = CastConversation.conversation.cast.thread_hash;
         const CLANKER_REPLY_PROMPT = CLANKER_PROMPT + `
 <about_token>
